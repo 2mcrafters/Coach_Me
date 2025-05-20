0f9298315e9b33a5b243a6049c48ed6a27bd6926
2mcrafters <2mcrafters@gmail.com>
2025-05-20 01:54:02 +0100

    Updated vite.config.js

--- front_web/vite.config.js
+++ front_web/vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),         // 👈 alias pour /src
-      '@redux': path.resolve(__dirname, '../redux'), // 👈 alias pour ton dossier redux externe
+      '@': path.resolve(__dirname, './src'),
+      '@redux': path.resolve(__dirname, '../redux'),
     },
   },
   server: {
     fs: {
-      allow: ['..'], // permet l'accès au dossier parent
+      allow: ['..'],
     },
   },
 });
